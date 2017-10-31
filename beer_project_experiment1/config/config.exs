5c1cfbf41dc4f31e134171bea27d13983250f115
Anthony green <green.an@husky.neu.edu>
2017-10-30 21:39:12 -0400

    use config to hold the accepted styles

--- beer_project_experiment1/config/config.exs
+++ beer_project_experiment1/config/config.exs
@@ -28,3 +28,11 @@ use Mix.Config
 # here (which is why it is important to import them last).
 #
 #     import_config "#{Mix.env}.exs"
+
+
+config :beer_project_experiment1,
+	accepted_styles: [
+		"IPA",
+		"Amber Ale",
+	]
+
