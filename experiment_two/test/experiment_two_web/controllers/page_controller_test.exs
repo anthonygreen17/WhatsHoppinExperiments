c2dd6473fe7d3d97b3609dfa763fb1f3c01a51a3
Max Corwin <maxcorwin@yahoo.com>
2017-10-31 00:28:05 -0400

    exp2: Initialized app for channels experiment

---
+++ experiment_two/test/experiment_two_web/controllers/page_controller_test.exs
@@ -0,0 +1,8 @@
+defmodule ExperimentTwoWeb.PageControllerTest do
+  use ExperimentTwoWeb.ConnCase
+
+  test "GET /", %{conn: conn} do
+    conn = get conn, "/"
+    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+  end
+end
