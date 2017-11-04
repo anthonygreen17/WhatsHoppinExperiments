7713c728b350cbacc4460767b3c7b3276b6f8168
Max Corwin <maxcorwin@yahoo.com>
2017-11-04 17:04:34 -0400

    exp2: Messages now fully working in JS and Channel

--- experiment_two/lib/experiment_two_web/channels/updates_channel.ex
+++ experiment_two/lib/experiment_two_web/channels/updates_channel.ex
@@ -16,27 +16,23 @@ defmodule ExperimentTwoWeb.UpdatesChannel do
     true
   end
 
-  # Channels can be used in a request/response fashion
-  # by sending replies to requests from the client
-  # def handle_in("ping", payload, socket) do
-  #   {:reply, {:ok, payload}, socket}
-  # end
-
-  # It is also common to receive messages from the client and
-  # broadcast to everyone in the current topic (updates:lobby).
-  # def handle_in("shout", payload, socket) do
-  #   broadcast socket, "shout", payload
-  #   {:noreply, socket}
-  # end
-
   def handle_in("message", payload, socket) do
+    Beer.create_message(payload)
     broadcast socket, "message", payload
     {:noreply, socket}
-    Beer.create_message(payload)
   end
 
   def handle_in("delete", payload, socket) do
-    Beer.delete_message(Beer.get_message!(payload["id"]));
+    msg = Beer.get_message!(payload["id"])
+    Beer.delete_message(msg)
+    broadcast socket, "delete", payload
+    {:noreply, socket}
+  end
+
+  def handle_in("update", payload, socket) do
+    msg = Beer.get_message!(payload["id"])
+    Beer.update_message(msg, %{content: payload["content"]})
+    broadcast socket, "update", payload
     {:noreply, socket}
   end
 end
